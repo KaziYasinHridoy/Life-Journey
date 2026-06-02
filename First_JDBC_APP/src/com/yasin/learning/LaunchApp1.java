df121cffe28be92bef761c3c4bd2b5eba0197401
KaziYasinHridoy <176918255+KaziYasinHridoy@users.noreply.github.com>
2026-06-03 04:22:46 +0600

    added First_JDBC_APP project

---
+++ First_JDBC_APP/src/com/yasin/learning/LaunchApp1.java
@@ -0,0 +1,24 @@
+package com.yasin.learning;
+import java.sql.*;
+public class LaunchApp1 {
+    static void main() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url="jdbc:mysql://localhost:3306/JDBC_learning";
+        String user="root";
+        String pass="secure";
+        Connection connection= DriverManager.getConnection(url,user,pass);
+
+        Statement statement = connection.createStatement();
+
+        String sql= "INSERT INTO student (id, name, age, city)\n" +
+                "VALUES \n" +
+                "(1, 'Arafat', 20, 'Dhaka'),\n" +
+                "(2, 'Nadia', 21, 'Chattogram'),\n" +
+                "(3, 'Rahim', 22, 'Khulna');";
+        int affectedRow=statement.executeUpdate(sql);
+        if (affectedRow==0) System.out.println("Unable to insert data");
+        else System.out.println("Data inserted successfully");
+        statement.close();
+        connection.close();
+    }
+}
