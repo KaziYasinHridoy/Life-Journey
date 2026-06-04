73e46eed95d72b73ee5d054b6dbbcd5d2ec40165
KaziYasinHridoy <176918255+KaziYasinHridoy@users.noreply.github.com>
2026-06-05 04:03:59 +0600

    LaunchApp2_with_utility_class added

---
+++ JDBC_APP_2/src/com/yasin/learning/LaunchApp2_with_utility_class/JdbcUtility.java
@@ -0,0 +1,41 @@
+package com.yasin.learning.LaunchApp2_with_utility_class;
+
+import java.sql.*;
+
+public class JdbcUtility {
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        String url      = "jdbc:mysql://localhost:3306/jdbc_learning";
+        String user     = "root";
+        String password = "secure";
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    public static void closeAllConnection(ResultSet rs, Statement statement, Connection connection) {
+        try {
+            if (rs         != null) rs.close();
+            if (statement  != null) statement.close();
+            if (connection != null) connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // If you don't have ResultSet (INSERT/UPDATE/DELETE)
+    public static void closeAllConnection(Statement statement, Connection connection) {
+        closeAllConnection(null, statement, connection);
+    }
+
+    // If you only need to close connection
+    public static void closeAllConnection(Connection connection) {
+        closeAllConnection(null, null, connection);
+    }
+}
