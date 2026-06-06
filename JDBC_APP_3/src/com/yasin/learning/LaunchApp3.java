40869b0433175d3c7887b73181f96d25c1ef5def
Kazi Yasin Hridoy <176918255+KaziYasinHridoy@users.noreply.github.com>
2026-06-07 03:10:35 +0600

    Refactor LaunchApp3 for batch processing and formatting

--- JDBC_APP_3/src/com/yasin/learning/LaunchApp3.java
+++ JDBC_APP_3/src/com/yasin/learning/LaunchApp3.java
@@ -9,8 +9,8 @@ public class LaunchApp3 {
 
         Connection connection = null;
         PreparedStatement ps = null;
-        ResultSet rs = null;
 
+        ResultSet rs = null;
         try {
 
             String sql =
@@ -20,25 +20,27 @@ public class LaunchApp3 {
 
             ps = connection.prepareStatement(sql);
 
-            Scanner scanner= new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in);
             System.out.println("Total Inputs:");
-            int n= scanner.nextInt();
+            int n = scanner.nextInt();
             for (int i = 0; i < n; i++) {
-                System.out.println("-------Student "+(i+1)+"-------");
+                System.out.println("-------> Student " +( i + 1 )+ " <-------");
                 System.out.println("ID:");
-                ps.setInt(1,scanner.nextInt() );
+                ps.setInt(1, scanner.nextInt());
                 System.out.println("Name:");
                 ps.setString(2, scanner.next());
                 System.out.println("Age:");
                 ps.setInt(3, scanner.nextInt());
                 System.out.println("City:");
                 ps.setString(4, scanner.next());
-                ps.executeUpdate();
+                ps.addBatch();
             }
+            scanner.close();
+            int[] affectedRows =ps.executeBatch();
             sql = "SELECT * FROM student";
 
             ps = connection.prepareStatement(sql);
-            boolean status= ps.execute();
+            boolean status = ps.execute();
             rs = ps.getResultSet();
             System.out.println("Added Information:");
             if (status) {
@@ -52,11 +54,9 @@ public class LaunchApp3 {
                 }
 
             }
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             JdbcUtility.closeAllConnection(
                     rs,
                     ps,
